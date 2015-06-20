b01b806d921165302291823b2b11aca0c3ca89c6
Joshua Newman <jonewman1020@gmail.com>
2015-06-20 15:09:45 -0700

    RPS redux

--- rockPaperScissors/rockPaperScissors.js
+++ rockPaperScissors/rockPaperScissors.js
@@ -1,20 +1,5 @@
 /*
-* Write a function that generates every sequence of throws a single
-* player could throw over a three-round game of rock-paper-scissors.
-*
-* Your output should look something like:
-*   [["rock", "rock", "rock"],
-*    ["rock", "rock", "paper"],
-*    ["rock", "rock", "scissors"],
-*    ["rock", "paper", "rock"],
-             ...etc...
-     ]
-*
-* Extra credit:
-*   - Make your function return answers for any number of rounds.
-* Example:
-* rockPaperScissors(5); // => [['rock', 'rock', 'rock', 'rock', 'rock'], etc...]
-*
+How many possible combos of RPS are there?
 */
 var options = ['rock', 'paper', 'scissors'];
 var combinations = [];
