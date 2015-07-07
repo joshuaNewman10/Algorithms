3b0faa8ea4ae290f736694003fbb0d84fb5a28b6
Joshua Newman <jonewman1020@gmail.com>
2015-07-07 11:58:29 -0700

    (Refactor) Reimplment insertionsort with hole instead of using splice

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -19,28 +19,30 @@ var insertionSort = function(array) {
 
   return array;
 };
-// var insertionSort = function(array) {
-//   for (var ix = 1; ix < array.length; ix++) {
-//     // Assume that array.slice(0, ix) is sorted.
 
-//     // Grab the current element
-//     var val = array[ix];
-//     var hole = ix;
 
-//     // Find the insertion point
-//     while (hole && val < array[hole - 1]) {
-//       array[hole] = array[hole - 1];
-//       hole -= 1;
-//     }
+var insertionSort = function(array) {
+  for (var ix = 1; ix < array.length; ix++) {
+    // Assume that array.slice(0, ix) is sorted.
 
-//     // Insert the element
-//     array[hole] = val;
-//     /* At this point, `val` has been inserted,
-//      * and array.slice(0, ix + 1) is now sorted.*/
-//   }
+    // Grab the current element
+    var val = array[ix];
+    var hole = ix;
 
-//   return array;
-// };
+    // Find the insertion point
+    while (hole && val < array[hole - 1]) {
+      array[hole] = array[hole - 1];
+      hole -= 1;
+    }
+
+    // Insert the element
+    array[hole] = val;
+    /* At this point, `val` has been inserted,
+     * and array.slice(0, ix + 1) is now sorted.*/
+  }
+
+  return array;
+};
 
 
 console.log(insertionSort([1,3,2, 18, 5]));
