3c69cb9f9fc7c632e16b6c7b7303d59f8b32fd8f
Joshua Newman <jonewman1020@gmail.com>
2015-07-24 10:28:41 -0700

    (refactor) Optimize bubble sort for early termination

--- sortingAlgorithms/bubbleSort.py
+++ sortingAlgorithms/bubbleSort.py
@@ -5,12 +5,17 @@ def swap(list, i, j):
 
 
 def bubble_sort(list):
+    did_sort = False
     for i in range(len(list)):
         currItem = list[i]
         for j in range(i, len(list)):
             comparrisonItem = list[j]
             if currItem > comparrisonItem:
+                did_sort = True
                 swap(list, i, j)
+        if not did_sort:
+            return list
+        did_sort = False
     return list
 
 print(bubble_sort([5, 3, 2, 1, 7, 8, 10]))
