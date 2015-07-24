123dbeabefc57938feb08b6f2b249a4de71cf36c
Joshua Newman <jonewman1020@gmail.com>
2015-07-23 18:57:56 -0700

    (finish brokenSequence

--- brokenSequence/brokenSequence.py
+++ brokenSequence/brokenSequence.py
@@ -1,6 +1,7 @@
 def find_missing_elem(sequence):
     num_set = set(int(x) for x in sequence.split(' '))
-    print(num_set)
+    true_seq = set(x for x in range(1, max(num_set)+1))
+    return ( true_seq.difference(num_set))
 
 
-find_missing_elem('1 2 4 5')
+print(find_missing_elem('1 2 4 5'))
