54252f1ee8ce426ca1800334e7198c15f7fc0de9
Joshua Newman <jonewman1020@gmail.com>
2015-07-25 19:54:04 -0700

    Towers Hanoi

---
+++ towersHanoi/towersHanoi.py
@@ -0,0 +1,20 @@
+def towers_hanoi(n):
+    a = []
+    b = []
+    c = []
+    for index in range(n):
+        a.append(index+1)
+
+    def recursive_func(n, source, dest, spare):
+        if (n == 1):
+            dest.append(source.pop())
+        else:
+            recursive_func(n-1, source, spare, dest)
+            dest.append(source.pop())
+            recursive_func(n-1, spare, dest, source)
+
+    recursive_func(n, a, c, b)
+    return c
+
+
+print(towers_hanoi(5))
